b4600fab0e292c683fedf6a857381e4784a5da84
MoreshkaYt <krivenoktimofey@gmail.com>
2025-12-06 15:02:12 +0700

    Add files via upload

---
+++ src/client/java/moreshka/moreshka/client/module/modules/Fullbright.java
@@ -0,0 +1,20 @@
+package moreshka.moreshka.client.module.modules;
+
+import moreshka.moreshka.client.module.Category;
+import moreshka.moreshka.client.module.Module;
+
+public class Fullbright extends Module {
+    public Fullbright() {
+        super("Fullbright", Category.RENDER);
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO: add brightness enabling logic
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO: add brightness disabling logic
+    }
+}
---
+++ src/client/java/moreshka/moreshka/client/module/modules/Hud.java
@@ -0,0 +1,20 @@
+package moreshka.moreshka.client.module.modules;
+
+import moreshka.moreshka.client.module.Category;
+import moreshka.moreshka.client.module.Module;
+
+public class Hud extends Module {
+    public Hud() {
+        super("Hud", Category.RENDER);
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO: add HUD enabling logic
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO: add HUD disabling logic
+    }
+}
